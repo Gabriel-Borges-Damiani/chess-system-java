083ee8b25f6ee00c5b9b5b3c3b64b7f9c43655a0
Gabriel Borges Damiani <damiani.gabri70@gmail.com>
2024-06-05 17:01:00 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hello world");
+
+		Position p = new Position(5, 2);
+			System.out.println(p);
+		
 	}
 
 }

472b0567f26db52df9450755e1583754d9889c59
Gabriel Borges Damiani <damiani.gabri70@gmail.com>
2024-06-06 17:25:23 -0300

    Starting to implement chess layer

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,15 @@
 package application;
 
-import boardgame.Position;
+import boardgame.Board;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Position p = new Position(5, 2);
-			System.out.println(p);
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 		
 	}
 
